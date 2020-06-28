6ec4ae7b3bf3ab7bc68a8918c6690602b136d52b
Andrés Grande <andresfernando.grande@e-campus.uab.cat>
2020-06-28 16:05:58 +0200

    Fix últimos detalles.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,13 +54,13 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Iniciar sesión') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
+{{--                                    <a class="btn btn-link" href="{{ route('password.request') }}">--}}
+{{--                                        {{ __('Forgot Your Password?') }}--}}
+{{--                                    </a>--}}
                                 @endif
                             </div>
                         </div>
